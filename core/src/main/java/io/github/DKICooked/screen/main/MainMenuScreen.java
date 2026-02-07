59af1e3fdeb5c881c03cdbcae7eb0f0bb0cf4132
Euno <jerickanthonyremo@yahoo.com>
2026-02-07 13:12:19 +0800

    added some stuff

--- core/src/main/java/io/github/DKICooked/screen/main/MainMenuScreen.java
+++ core/src/main/java/io/github/DKICooked/screen/main/MainMenuScreen.java
@@ -1,31 +1,46 @@
-package io.github.DKICooked.gui;
+package io.github.DKICooked.screen.main;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Scaling;
+import io.github.DKICooked.screen.BaseScreen;
 
 public class MainMenuScreen extends BaseScreen {
+    private int selectedIndex = 0;
+    private ImageButton[] buttons;
     private final Texture startText;
+    private final Texture startTextHov;
     private final Texture tutText;
+    private final Texture tutTextHov;
     private final Texture setText;
+    private final Texture setTextHov;
     private final Texture exitText;
+    private final Texture exitTextHov;
     private final Texture titleText;
     private final Texture subTitleText;
-
 
     public MainMenuScreen() {
         super();
         startText = new Texture(Gdx.files.internal("Start.png"));
+        startTextHov = new Texture(Gdx.files.internal("Start_pressed.png"));
         tutText = new Texture(Gdx.files.internal("tutorial.png"));
+        tutTextHov = new Texture(Gdx.files.internal("Tutorial_pressed.png"));
         setText = new Texture(Gdx.files.internal("settings.png"));
+        setTextHov = new Texture(Gdx.files.internal("Setting_pressed.png"));
+
         exitText = new Texture(Gdx.files.internal("exit.png"));
+        exitTextHov = new Texture(Gdx.files.internal("exit_pressed.png"));
 
         titleText = new Texture(Gdx.files.internal("DonkeyKong.png"));
         subTitleText = new Texture(Gdx.files.internal("Infinity.png"));
@@ -41,23 +56,34 @@ public class MainMenuScreen extends BaseScreen {
         subTitle.setScaling(Scaling.fit);
 
         table.top().center();
-        table.add(title).width(Gdx.graphics.getWidth() * 0.70f).bottom().padBottom(-100).row();
-        table.add(subTitle).width(Gdx.graphics.getWidth() * 0.9f).top().padBottom(50).row();
+        table.add(title).width(Gdx.graphics.getWidth() * 0.37f).padBottom(-133).row();
+        table.add(subTitle).width(Gdx.graphics.getWidth() * 0.5f).padBottom(50).row();
+
 
         ImageButton.ImageButtonStyle startStyle = new ImageButton.ImageButtonStyle();
         startStyle.imageUp = new TextureRegionDrawable( new TextureRegion(startText));
+        startStyle.imageOver = new TextureRegionDrawable( new TextureRegion(startTextHov));
+        startStyle.imageDown = new TextureRegionDrawable( new TextureRegion(startTextHov));
+
         ImageButton startButton = new ImageButton(startStyle);
+        startButton.setTouchable(Touchable.enabled);
 
         ImageButton.ImageButtonStyle tutStyle = new ImageButton.ImageButtonStyle();
         tutStyle.imageUp = new TextureRegionDrawable( new TextureRegion(tutText));
+        tutStyle.imageOver = new TextureRegionDrawable( new TextureRegion(tutTextHov));
+        tutStyle.imageDown = new TextureRegionDrawable( new TextureRegion(tutTextHov));
         ImageButton tutButton = new ImageButton(tutStyle);
 
         ImageButton.ImageButtonStyle settStyle = new ImageButton.ImageButtonStyle();
         settStyle.imageUp = new TextureRegionDrawable( new TextureRegion(setText));
+        settStyle.imageOver = new TextureRegionDrawable( new TextureRegion(setTextHov));
+        settStyle.imageDown = new TextureRegionDrawable( new TextureRegion(setTextHov));
         ImageButton settButton = new ImageButton(settStyle);
 
         ImageButton.ImageButtonStyle exitStyle = new ImageButton.ImageButtonStyle();
         exitStyle.imageUp = new TextureRegionDrawable( new TextureRegion(exitText));
+        exitStyle.imageOver = new TextureRegionDrawable( new TextureRegion(exitTextHov));
+        exitStyle.imageDown = new TextureRegionDrawable( new TextureRegion(exitTextHov));
         ImageButton exitButton = new ImageButton(exitStyle);
 
         startButton.getImage().setScaling(Scaling.fit);
@@ -65,11 +91,19 @@ public class MainMenuScreen extends BaseScreen {
         settButton.getImage().setScaling(Scaling.fit);
         exitButton.getImage().setScaling(Scaling.fit);
 
-        table.add(startButton).width(170).height(80).padTop(-60).center().row();
-        table.add(tutButton).width(200).height(80).padTop(-20).center().row();
-        table.add(settButton).width(200).height(80).padTop(-20).padBottom(50).center().row();
-        table.add(exitButton).width(100).height(40).center().row();
+        table.add(startButton).width(120).height(25).padTop(-60).center().row();
+        table.add(tutButton).width(140).height(50).padTop(-20).center().row();
+        table.add(settButton).width(140).height(50).padTop(-14).padBottom(50).center().row();
+        table.add(exitButton).width(140).height(30).center().row();
 
+        subTitle.addAction(
+            Actions.forever(
+                Actions.sequence(
+                    Actions.moveBy(0, 10, 0.7f),
+                    Actions.moveBy(0, -10, 0.7f)
+                )
+            )
+        );
         startButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -97,15 +131,19 @@ public class MainMenuScreen extends BaseScreen {
                 System.out.println("Exit Button was clicked");
             }
         });
-
     }
+
     @Override
     public void dispose() {
         super.dispose();
         startText.dispose();
+        startTextHov.dispose();
         tutText.dispose();
+        tutTextHov.dispose();
         setText.dispose();
+        setTextHov.dispose();
         exitText.dispose();
+        exitTextHov.dispose();
         titleText.dispose();
         subTitleText.dispose();
     }
--- lwjgl3/src/main/java/io/github/DKICooked/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/DKICooked/lwjgl3/Lwjgl3Launcher.java
@@ -1,5 +1,6 @@
 package io.github.DKICooked.lwjgl3;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.DKICooked.Main;
@@ -17,6 +18,8 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        Graphics.DisplayMode displayMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+
         configuration.setTitle("Donkey Kong Infinity");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
@@ -28,7 +31,8 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 
-        configuration.setWindowedMode(800, 600);
+        configuration.setWindowedMode(1000, 800);
+        configuration.setResizable(true);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
